3bf6f6c3274170ad6a8d83d1598f62935a565c41
CaioLuppo <luppocode@outlook.com>
2022-09-06 17:29:37 -0300

    Criando componentes

---
+++ lib/components/navegador.dart
@@ -0,0 +1,55 @@
+// Tela principal do aplicativo
+
+import "package:flutter/material.dart";
+import 'package:ta_chegando/pages/onibus.dart';
+
+class Navegador extends StatefulWidget {
+  const Navegador({super.key});
+
+  @override
+  State<Navegador> createState() => _NavegadorState();
+}
+
+class _NavegadorState extends State<Navegador> {
+  /// Controlador das páginas. Navega por índice.
+  PageController controladorPageView = PageController();
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: PageView(
+        controller: controladorPageView,
+        children: [
+          OnibusPage(),
+          Text("Oi"),
+        ],
+      ),
+      bottomNavigationBar: AnimatedBuilder(
+          animation: controladorPageView,
+          builder: (context, snapshot) {
+            return BottomNavigationBar(
+              currentIndex: controladorPageView.page?.round() ?? 0,
+              onTap: (index) => controladorPageView.animateToPage(
+                index,
+                duration: const Duration(milliseconds: 500),
+                curve: Curves.easeInCubic,
+              ),
+              items: const [
+                BottomNavigationBarItem(
+                  icon: Icon(Icons.directions_bus_filled),
+                  label: "Ônibus",
+                ),
+                BottomNavigationBarItem(
+                  icon: Icon(Icons.map),
+                  label: "Mapa",
+                ),
+                BottomNavigationBarItem(
+                  icon: Icon(Icons.share_location_rounded),
+                  label: "Pontos",
+                ),
+              ],
+            );
+          }),
+    );
+  }
+}
