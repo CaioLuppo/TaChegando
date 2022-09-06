cc1168ce6689a76dccb0f347c07fc4f22cb2124e
CaioLuppo <luppocode@outlook.com>
2022-09-06 17:30:13 -0300

    Definindo estilos

---
+++ lib/styles/text.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class TituloBranco extends TextStyle {
+  const TituloBranco()
+      : super(
+          fontSize: 24,
+          fontWeight: FontWeight.bold,
+          color: Colors.white,
+        );
+}
